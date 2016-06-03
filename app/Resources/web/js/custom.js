5bf7a2d9bc5402079ab1b35468c3fa24d9c1f9af
Julian <Yulyan.Misyukevich@r-stylelab.com>
2016-06-03 19:03:43 +0300

    Update packages, gulp for babel and for libs

---
+++ app/Resources/web/js/custom.js
@@ -0,0 +1,5 @@
+// EXAMPLE
+ReactDOM.render(
+<h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,45 @@ var less = require('gulp-less');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var env = process.env.GULP_ENV;
+var babel = require('gulp-babel');
 
-gulp.task('js', function () {
-    return gulp.src(['bower_components/jquery/dist/jquery.js',
+// TODO: implement
+var path = {
+    src: {
+        js: 'app/Resources/web/js/*.js',
+        css: 'app/Resources/web/css/*.js'
+    },
+    dist: {
+        js: 'web/js/',
+        css: 'web/css/'
+    }
+};
+
+gulp.task('js:libs:build', function () {
+    return gulp.src([
+        'bower_components/jquery/dist/jquery.js',
         'bower_components/bootstrap/dist/js/bootstrap.js',
-        'app/Resources/web/js/**/*.js'])
-        .pipe(concat('scripts.js'))
+        'bower_components/react/react.js',
+        'bower_components/react/react-dom.js',
+        'bower_components/react/react-with-addons.js'
+    ])
+        .pipe(concat('libs.js'))
         .pipe(gulpif(env === 'prod', uglify()))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('web/js'));
 });
 
-gulp.task('css', function () {
+gulp.task('js:babel:build', function () {
+    return gulp.src('app/Resources/web/js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('web/js'));
+});
+
+gulp.task('js:build', ['js:libs:build', 'js:babel:build']);
+
+gulp.task('css:build', function () {
     return gulp.src([
         'bower_components/bootstrap/dist/css/bootstrap.css',
         'app/Resources/web/less/**/*.less'])
@@ -28,4 +55,14 @@ gulp.task('css', function () {
         .pipe(gulp.dest('web/css'));
 });
 
-gulp.task('default', ['js', 'css']);
+gulp.task('js:watch', function () {
+    return gulp.watch([path.src.js], ['js:build']);
+});
+
+gulp.task('css:watch', function () {
+    return gulp.watch([path.src.css + '/**'], ['css:build']);
+});
+
+gulp.task('default', ['js:build', 'css:build']);
+
+gulp.task('watch', ['js:watch', 'css:watch']);
